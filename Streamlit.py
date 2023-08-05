1a0b290c6e12bbcc33240c1e6a368e8b308d2d1e
nirajvesaokar343 <113465384+nirajvesaokar343@users.noreply.github.com>
2023-08-05 11:30:36 -0400

    Add files via upload

---
+++ Streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import nltk
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics import classification_report, accuracy_score
+
+st.title('Amazon Alexa Sentiment Analysis')
+
+# Load your preprocessed dataset here
+df = pd.read_csv('amazon_alexa.csv')
+
+# ... Code for preprocessing and model training ...
+
+# Define the Streamlit app interface
+st.subheader('Enter a review:')
+user_input = st.text_input('')
+
+if st.button('Analyze Sentiment'):
+    # ... Code to preprocess user input and make predictions ...
+
+    st.subheader('Prediction:')
+    if prediction == 1:
+        st.write('Positive sentiment')
+    else:
+        st.write('Negative sentiment')
